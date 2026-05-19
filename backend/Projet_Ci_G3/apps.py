4d647d6428330e73d4ae1a0bd13e537827cbc2a8
fanta <kadifanta6@gmail.com>
2026-05-19 15:49:14 +0000

    init: structure projet React Vite + Django

---
+++ backend/Projet_Ci_G3/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProjetCiG3Config(AppConfig):
+    name = 'Projet_Ci_G3'
---
+++ backend/Projet_site_Web_Vitrine/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProjetSiteWebVitrineConfig(AppConfig):
+    name = 'Projet_site_Web_Vitrine'
